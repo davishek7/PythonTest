e4d921376be035308df1d0979dcbdddcf9b8908a
Avishek Das <davishek7@gmail.com>
2020-09-14 17:33:15 +0530

    version 2

--- Test.py
+++ Test.py
@@ -1,4 +1,14 @@
 a=int(input('enter a number'))
 b=int(input('enter another number'))
-c=a+b
-print(c)
+if a>b:
+    c=a-b
+    print('a is greater than b')
+    print(c)
+elif a==b:
+    print('a & b are both equal')
+    c=a-b
+    print(c)
+else:
+    c=b-a
+    print('b is greater than a')
+    print(c)
