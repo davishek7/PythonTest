511508ec67b9ea4129703facadeef2598e9ca72e
Avishek Das <davishek7@gmail.com>
2020-09-20 17:08:58 +0530

    Function Upgrade

--- PrimeTest.py
+++ PrimeTest.py
@@ -1,10 +1,15 @@
-lower=int(input('Enter the lower range'))
-upper=int(input('Enter the upper range'))
-
-for x in range(lower,upper):
-    if x>1:
-        for i in range(2,x):
-            if x%i==0:
-                break
-        else:
-            print(x)
+def prime_test(x,y):
+    for i in range(x,y):
+        if i>1:
+            for j in range(2,x):
+                if i%j==0:
+                    break
+            else:
+                print(i)
+    return(i)
+def main():
+    lower=int(input("Enter the lower range"))
+    upper=int(input("Enter the upper range"))
+    prime_test(lower,upper)
+if __name__=='__main__':
+    main()               
--- prime.py
+++ prime.py
@@ -1,10 +1,16 @@
-x=int(input('Enter a number '))
-if x>1:
+def prime(x):
     for i in range(2,x):
         if x%i==0:
             print('Not a Prime number')
             break
     else:
         print('Prime number')
-else:
-    print("Please enter a value greater than 1")
+    return(x)
+def main():
+    a=int(input('Enter a number '))
+    if a>1:
+        prime(a)
+    else:
+        print("Enter a value greater than 1")
+if __name__=='__main__':
+    main()
